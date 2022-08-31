e672d0e1d466602c4feb22037ce825404765004e
KxngMatthew <KxngMatthew@users.noreply.github.com>
2022-08-30 20:18:59 -0500

    Database Migrations and table configurations

--- LeaveManagementApp.Web/Data/Migrations/20220830234207_AddedLeaveTables.Designer.cs
+++ LeaveManagementApp.Web/Data/Migrations/20220830234207_AddedLeaveTables.Designer.cs
@@ -4,6 +4,7 @@ using LeaveManagementApp.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LeaveManagementApp.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220830234207_AddedLeaveTables")]
+    partial class AddedLeaveTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
