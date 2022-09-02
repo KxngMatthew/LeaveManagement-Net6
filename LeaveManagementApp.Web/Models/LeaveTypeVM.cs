6fd1594806e712598345991eb0a2948a1c4a62d5
KxngMatthew <KxngMatthew@users.noreply.github.com>
2022-09-02 00:38:39 -0500

    Index page modifications and other updates

--- LeaveManagementApp.Web/Models/LeaveTypeVM.cs
+++ LeaveManagementApp.Web/Models/LeaveTypeVM.cs
@@ -7,10 +7,12 @@ namespace LeaveManagementApp.Web.Models
         public int Id { get; set; }
         [Display(Name = "Leave Type Name")]
         [Required]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         [Display(Name = "Default Number of Days")]
         [Required]
         public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
     }
 }
